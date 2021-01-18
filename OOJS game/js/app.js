1f36bca8d9767dc9a3a936883e6aff8285dc56de
Bert Witzel <bwitzel82@gmail.com>
2021-01-18 17:32:43 -0500

    update all js files to completed project

--- OOJS game/js/app.js
+++ OOJS game/js/app.js
@@ -1,11 +1,17 @@
 const game = new Game();
 
-const beginGame = document.getElementById('begin-game');
 /** 
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-beginGame.addEventListener('click', () => {
+document.getElementById('begin-game').addEventListener('click', function(){
     game.startGame();
     this.style.display = 'none';
     document.getElementById('play-area').style.opacity = '1';
-})
+});
+
+/** 
+ * Listen for keyboard presses
+ */
+document.addEventListener('keydown', function(event){
+    game.handleKeydown(event);
+});
